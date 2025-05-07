79f0cfb52ec34660b5c069b989a87b49487ea921
llj <llj@Mac-mini.local>
2025-05-07 14:40:57 +0800

    modify

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template
 from routes_timeline import timeline_bp
 from routes_character import character_bp
+from utils import story_map, load_story_map
 
 app = Flask(__name__)
 
@@ -10,7 +11,8 @@ app.register_blueprint(character_bp)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', storys = load_story_map().items())
 
 if __name__ == '__main__':
+    # print(load_story_map())
     app.run(host='0.0.0.0', port=5001, debug=True)
--- routes_timeline.py
+++ routes_timeline.py
@@ -22,7 +22,9 @@ def save_data(data, file_path):
 
 @timeline_bp.route('/story/<story_id>/timeline')
 def index(story_id):
-    return render_template('timeline.html')
+    if story_id not in story_map:
+        return "Invalid story ID", 404
+    return render_template('timeline.html', storyName = story_map[story_id])
 
 @timeline_bp.route('/story/<story_id>/event/<int:event_id>', methods=['PATCH'])
 def update_event(story_id,event_id):
