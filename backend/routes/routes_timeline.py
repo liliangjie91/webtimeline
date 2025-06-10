b8c9a6ac2b3aaff9fee0a93a3571078c99dccbb0
llj <llj@Mac-mini.local>
2025-06-10 22:43:59 +0800

    network init

--- backend/routes/routes_timeline.py
+++ backend/routes/routes_timeline.py
@@ -11,3 +11,8 @@ def index():
     if story_id not in story_map:
         return "Invalid story ID", 404
     return render_template('timeline.html', storyName = story_map[story_id])
+
+@timeline_bp.route('/story/network')
+def network():
+    story_id = request.args.get('story_id', '1')
+    return render_template('network.html')
